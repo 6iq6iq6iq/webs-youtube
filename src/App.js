ae1d927aca99c0e042c239db41d168292285b079
6iq6iq6iq <6iq6iq6iq@gmail.com>
2024-03-26 21:04:23 +0900

    설명 읽어
    이것은 현재 깃허브에 저장되어 있는 파일임.
    내가 지금 로컬에서 수정하는 중 _ 데스크탑

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      
+        <div>이것은 현재 깃허브에 저장되어 있는 파일임.</div>
+        <div>내가 지금 로컬에서 수정하는 중 _ 데스크탑</div>
+
+    </>
+
   )
 }
 
