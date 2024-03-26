9b464eeabe0e5aaf46dc297fa715ec9273f0a436
6iq6iq6iq <6iq6iq6iq@gmail.com>
2024-03-26 21:07:03 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>이것은 현재 깃허브에 저장되어 있는 파일임내가 지금 로컬에서 수정하는 중</div>
+    <div>이것은 현재 깃허브에 저장되어 있는 파일임 
+      내가 지금 로컬에서 수정하는 중
+      2일째 수정중 css추가 페이지 추가</div>
 
   )
 }
