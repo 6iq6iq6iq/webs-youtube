5a124f3c052224a5f15087328d73682d7734c1a4
6iq6iq6iq <164736954+6iq6iq6iq@users.noreply.github.com>
2024-03-30 11:03:10 +0900

    페이지 만들기
    0330_투썸에서

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Today from './pages/Today'
+import Developer from './pages/Developer'
+import Webd from './pages/Webd'
+import Website from './pages/Website'
+import Gsap from './pages/Gsap'
+import Port from './pages/Port'
+import Youtube from './pages/Youtube'
+import Channel from './pages/Channel'
+import Video from './pages/Video'
+import Search from './pages/Search'
+import Not from './pages/Not'
+
+
+
 
 const App = () => {
   return (
-    <div>이것은 현재 깃허브에 저장되어 있는 파일임 
-      내가 지금 로컬에서 수정하는 중
-      2일째 수정중 css추가 페이지 추가</div>
-
+    <BrowserRouter>
+        <Routes>
+                <Route path='/' element={<Home/>} />
+                <Route path='/today' element={<Today/>} />
+                <Route path='/developer' element={<Developer/>} />
+                <Route path='/webd' element={<Webd/>} />
+                <Route path='/website' element={<Website/>} />
+                <Route path='/gsap' element={<Gsap/>} />
+                <Route path='/port' element={<Port/>} />
+                <Route path='/youtube' element={<Youtube/>} />
+                <Route path='/channel/:channelID' element={<Channel/>} />
+                <Route path='/video/:videoID' element={<Video/>} />
+                <Route path='/search/:searchID' element={<Search/>} />
+                <Route path='/*' element={<Not/>}/>
+        </Routes>
+    </BrowserRouter>
   )
 }
 
